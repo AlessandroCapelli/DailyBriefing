b58198401c93421452607c1d25ada782bfad1d5d
Alessandro Capelli <AlessandroCapelli@users.noreply.github.com>
2026-04-16 02:32:48 +0200

    Rimuovi ticker dal breadcrumb deep, mantieni frecce a destra

--- nav.js
+++ nav.js
@@ -130,8 +130,6 @@
 				var uniqueTickers = Object.keys(latestByTicker).sort();
 				var idxT = uniqueTickers.indexOf(pageTicker);
 
-				addEl("span", "db-s", "/");
-				addEl("span", "db-a", pageTicker);
 				addEl("span", "db-sp");
 
 				// Ticker navigation arrows
